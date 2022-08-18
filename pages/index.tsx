09d35a87d6d79e999fa77d638b1392ab7d2bfd62
maxyou <hyyou2010@gmail.com>
2022-08-18 22:51:46 +0800

    xor and de-xor worked

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,84 +2,82 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+function encryptXor(text:string, key:string) {
+    var result = '';
+
+    for (var i = 0; i < text.length; i++) {
+
+        console.log('text char:'+ text.charCodeAt(i).toString(16))
+        console.log('key char:'+ key.charCodeAt(i % key.length).toString(16))
+
+        var v =  (text.charCodeAt(i) ^ key.charCodeAt(i % key.length)).toString(16)
+        if(v.length == 1){
+          v = '0'+v
+        }
+        console.log('v:'+v)
+        
+        result += v
+        console.log('res:'+result)
+    }
+    return result;
+}
+
+function deXor(hexStr:string, key:string) {
+
+  var result = '';
+
+  for (var i = 0; i < hexStr.length; i++,i++) {
+
+    const v = hexStr.substring(i, i+2)
+    console.log('bv:'+v)
+    const hexNum = parseInt(v, 16)
+
+    var vv =  String.fromCharCode(hexNum ^ key.charCodeAt((i/2) % key.length))
+
+    result += vv
+
+  }
+
+  return result
+  
+}
+
 const Home: NextPage = () => {
+
+  const a = 'aaa@gmail.com'
+  const k = '000xxxfff789'
+
+  const b = encryptXor(a, k)
+  console.log('b:'+b)
+
+  const c = deXor(b, k)
+
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>purebbs-nextjs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className='bg-gray-200 w-screen h-screen grid gap-2 place-content-center'>
+        <div className='bg-red-500 w-full h-16 p-2'>
+          {a}
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+        <div className='bg-green-500 w-full h-16'>
+          {b}
+        </div>        
+        <div className='bg-blue-500 w-full h-16'>
+          {c}
+        </div>
+        {/* <div className='bg-yellow-500 w-32 h-8'>
+          测试
+        </div>
+        <div className='bg-orange-500 w-32 h-8'>
+          测试
+        </div> */}
+    </div>      
+  </>
   )
 }
 
